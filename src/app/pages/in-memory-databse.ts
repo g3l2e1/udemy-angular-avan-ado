ca01b8ed4149736ad49ee5adbf8dcfd42f918db4
Francisco Gleyson Duarte Barroso <gleyson@asasulfabrica.com.br>
2019-02-05 16:55:32 -0300

    Adicionando category model

--- src/app/pages/in-memory-databse.ts
+++ src/app/pages/in-memory-databse.ts
@@ -1,8 +1,9 @@
 import { InMemoryDbService } from "angular2-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
 
 export class InMemoryDatabaseService implements InMemoryDbService {
   createDb(){
-    const categories = [
+    const categories: Category[] = [
       { id: 1, nome: "Moradia", descricao: "pagamentos de contas da casa" },
       { id: 2, nome: "Saúde", descricao: "Plano de sáude e remédios" },
       { id: 3, nome: "Lazer", descricao: "Cinema, praia, parque, etc." },
